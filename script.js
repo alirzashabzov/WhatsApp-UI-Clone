3e9842620a07f9c7bedef4d861808923abaa0068
Alirza Shabazov <127769153+alirzashabzov@users.noreply.github.com>
2023-03-18 20:48:02 +0400

    WhatsApp UI Clone

---
+++ script.js
@@ -0,0 +1,35 @@
+let back = document.querySelector('.back');
+let chatBox = document.querySelector('.chatBox');
+let open = document.querySelector('.open');
+
+
+
+back.onclick=function name(params) {
+    chatBox.classList.add('hide')
+}
+
+back.onclick=function name(params) {
+    chatBox.classList.add('hide')
+}
+open.onclick = function name(params) {
+    chatBox.classList.remove('hide')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
